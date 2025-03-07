825b6f7ee0b904bcbb1831337b1ef7fae6c9a5bb
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-03-07 21:20:44 +0530

    Update register.php

--- function/register.php
+++ function/register.php
@@ -1,15 +1,27 @@
 <?php
+
     include "../config.php";
     require 'vendor/autoload.php';    
     use Firebase\JWT\JWT;
     use Firebase\JWT\Key;
 
     if($_SERVER["REQUEST_METHOD"] === "POST"){
+        $data = json_decode(file_get_contents("php://input"), true);
+        $email = $data['email'];
+        $password = password_hash($data['password'], PASSWORD_DEFAULT);
 
+        $checkstmt = $pdo->prepare("SELECT * FROM users WHERE email = ?");
+        $checkstmt->execute([$email]);
+
+        if($checkstmt){
+            
+        }
+        else{
+            echo json_encode(["error" => "User Cannot Find..."]);
+        }
     }
     else{
         echo json_encode(["error" => "Check Request Method Please..."]);
     }
-
 
 ?>
