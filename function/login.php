72c64ab06c082c40e5d64ff919f5445b2ff7e9b0
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-03-08 05:44:45 +0530

    Update

--- function/login.php
+++ function/login.php
@@ -1,24 +1,29 @@
-<?php 
+<?php
+
     include "../config.php";
+    require '../vendor/autoload.php';    
+    use Firebase\JWT\JWT;
+    use Firebase\JWT\Key;
 
     if($_SERVER["REQUEST_METHOD"] === "POST"){
         $data = json_decode(file_get_contents("php://input"), true);
-        $username = $data['username'];
         $email = $data['email'];
         $password = password_hash($data['password'], PASSWORD_DEFAULT);
 
         $checkstmt = $pdo->prepare("SELECT * FROM users WHERE email = ?");
         $checkstmt->execute([$email]);
+        $user = $checkstmt->fetch(PDO::FETCH_ASSOC);
 
-        $checkuser = $checkstmt->fetch(PDO::FETCH_ASSOC);
-        
-        if($checkuser){
-            echo json_encode(["error" => "User Already Registerd"]);
+        if ($user && password_verify($password, $user['password'])) {
+            $payload = [
+                'email' => $user['email'],
+                'exp' => time() + (60 * 60)
+            ];
+            $token = JWT::encode($payload, $secret_key, 'HS256');
+            echo json_encode(["message" => "Login successful", "token" => $token]);
         }
         else{
-            $stmt = $pdo->prepare("INSERT INTO users (username, email, password, is_active) VALUES (?, ?, ?, ?)");
-            $stmt->execute([$username, $email, $password, "0"]);
-            echo json_encode(["message" => "Registration successful"]);
+            echo json_encode(["error" => "User Cannot Find..."]);
         }
     }
     else{
--- function/register.php
+++ function/register.php
@@ -1,23 +1,24 @@
-<?php
-
+<?php 
     include "../config.php";
-    require 'vendor/autoload.php';    
-    use Firebase\JWT\JWT;
-    use Firebase\JWT\Key;
 
     if($_SERVER["REQUEST_METHOD"] === "POST"){
         $data = json_decode(file_get_contents("php://input"), true);
+        $username = $data['username'];
         $email = $data['email'];
         $password = password_hash($data['password'], PASSWORD_DEFAULT);
 
         $checkstmt = $pdo->prepare("SELECT * FROM users WHERE email = ?");
         $checkstmt->execute([$email]);
 
-        if($checkstmt){
-            
+        $checkuser = $checkstmt->fetch(PDO::FETCH_ASSOC);
+        
+        if($checkuser){
+            echo json_encode(["error" => "User Already Registerd"]);
         }
         else{
-            echo json_encode(["error" => "User Cannot Find..."]);
+            $stmt = $pdo->prepare("INSERT INTO users (username, email, password, is_active) VALUES (?, ?, ?, ?)");
+            $stmt->execute([$username, $email, $password, "0"]);
+            echo json_encode(["message" => "Registration successful"]);
         }
     }
     else{
